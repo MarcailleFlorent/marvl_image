f270ad61475ca04432e80a7645c5477979e0ce72
FLORENT MARCAILLE <florent.marcaille@atos.net>
2017-10-30 11:12:07 +0100

    add of coca comparison image + first comparison

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,4 @@
-import static org.bytedeco.javacpp.opencv_core.*;
+/**import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_highgui.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
@@ -16,7 +16,27 @@ import org.bytedeco.javacpp.opencv_shape;
 import org.bytedeco.javacpp.opencv_xfeatures2d.SIFT;
 
 import static org.bytedeco.javacpp.opencv_features2d.drawKeypoints;
+import static org.bytedeco.javacpp.opencv_features2d.drawMatches; **/
+
+
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_highgui.*;
+import static org.bytedeco.javacpp.opencv_features2d.drawKeypoints;
 import static org.bytedeco.javacpp.opencv_features2d.drawMatches;
+
+import static org.bytedeco.javacpp.opencv_core.*;
+import java.util.Arrays;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.opencv_calib3d;
+import org.bytedeco.javacpp.opencv_core.DMatchVector;
+import org.bytedeco.javacpp.opencv_core.KeyPointVector;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Scalar;
+import org.bytedeco.javacpp.opencv_features2d.BFMatcher;
+import org.bytedeco.javacpp.opencv_features2d.DrawMatchesFlags;
+import org.bytedeco.javacpp.opencv_shape;
+import org.bytedeco.javacpp.opencv_xfeatures2d.SIFT;
 
 
 public class Hello {
@@ -26,55 +46,55 @@ public class Hello {
 	*	@param	args
 	*/
 	public	static	void	main(String[]	args)	{	
-		//Mat img = loadImg();
-		//Mat grayImg = putInGray(img);
-		
-		// displaySiftofImage(img);
-		
-		
-		Mat[] images = new Mat[2];
-		images[0] = loadImg();
-		images[1] = loadImg();
-		
-		displayImg("fff", images[1]);
-		
-		KeyPointVector[] keyPoints = new KeyPointVector[2];
-		
 		int nFeatures = 0; // Le nombre de meilleurs caractéristiques à retenir. Les caractéristiques sont classées par leurs scores (mesuré dans l'algorithme SIFT comme le contraste local).
 		int nOctaveLayers = 3; // Nombre de couche dans chaque octave (3 est la valeur utilisée avec D.Lowe). Le nombre d'octave est calculé automatiquement à partir de la résolution de l'image.
 		double contrastThreshold = 0.03; // Seuil de contraste utilisé pour filtrer les caractéristiques des régions à faible contraste. Plus le seuil est important, moins les caractéristiques sont produites par le détecteur.
 		int edgeThresold = 10; // Seuil utilisé pour filtrer les caractéristiques de pointe. Plus la valeur est importante moins les caractéristiques sont filtrées
 		double sigma = 1.6; // sigma gaussien appliqué à l'image d'entrée à l'octave \ # 0. Réduire le nombre si image capturée est de faible qualité
+		
+		Mat[] images = new Mat[2];
+		//KeyPointVector[] keyPoints = new KeyPointVector[2];
+		KeyPointVector keyPoints1 = new KeyPointVector();
+		KeyPointVector keyPoints2 = new KeyPointVector();
+		
 		Mat[] descriptors = null;
 		
-		Loader.load(opencv_calib3d.class);
-		Loader.load(opencv_shape.class);
-		
 		SIFT sift;
-		sift = SIFT.create(nFeatures,nOctaveLayers,contrastThreshold,edgeThresold,sigma);
-
-		sift.detect(images[0], keyPoints[0]);
-		sift.detect(images[1], keyPoints[1]);
 		
-		descriptors[0] = new Mat();
-		descriptors[1] = new Mat();
-		
-		sift.compute(images[0], keyPoints[0], descriptors[0]);
-		sift.compute(images[1], keyPoints[1], descriptors[1]);
-		
-		// Match descriptors
 		BFMatcher matcher = new BFMatcher(NORM_L2,false);
 		DMatchVector matches = new DMatchVector();
 		
-		matcher.match(descriptors[0], descriptors[1], matches);
+		images[0] = loadImg("Coca_1.jpg");
+		images[1] = loadImg("Coca_2.jpg");
+				
+		Loader.load(opencv_calib3d.class);
+		Loader.load(opencv_shape.class);
+				
+		sift = SIFT.create(nFeatures,nOctaveLayers,contrastThreshold,edgeThresold,sigma);
+
+		sift.detect(images[0], keyPoints1);
+		sift.detect(images[1], keyPoints2);
+		
+		
+		Mat descriptors1 = new Mat();
+		Mat descriptors2 = new Mat();
+		
+		//descriptors[0] = new Mat();
+		//descriptors[1] = new Mat();
+		
+		sift.compute(images[0], keyPoints1, descriptors1);
+		sift.compute(images[1], keyPoints2, descriptors2);
+		
+		// Match descriptors
+		matcher.match(descriptors1, descriptors2, matches);
 		
 		DMatchVector bestMatches = selectBestMatcher(matches, 10);
 		
 		// Draw Matches
 		Mat imageMatches = new Mat();
-		BytePointer mask = null;
+		//BytePointer mask = null;
 		
-		drawMatches(images[0], keyPoints[0], images[1], keyPoints[1], bestMatches, imageMatches);
+		drawMatches(images[0], keyPoints1, images[1], keyPoints2, bestMatches, imageMatches);
 		displayImg("Comparison", imageMatches);
 	}
 	
@@ -82,7 +102,7 @@ public class Hello {
 	
 	private static Mat putInGray(Mat image) {
 		Mat gray = new Mat(image.size());
-		cvtColor(image, gray, CV_BGR2GRAY);
+		//cvtColor(image, gray, CV_BGR2GRAY);
 		return gray;
 	}
 
@@ -112,8 +132,8 @@ public class Hello {
 	
 	private static void displaySiftComparison() {
 		Mat[] images = new Mat[2];
-		images[0] = loadImg();
-		images[1] = loadImg();
+		images[0] = loadImgLena();
+		images[1] = loadImgLena();
 		
 		KeyPointVector[] keyPoints = new KeyPointVector[2];
 		
@@ -157,7 +177,7 @@ public class Hello {
 		
 		// Draw Matches
 		Mat imageMatches = new Mat();
-		BytePointer mask = null;
+		//BytePointer mask = null;
 		
 		drawMatches(images[0], keyPoints[0], images[1], keyPoints[1], bestMatches, imageMatches);
 		displayImg("Comparison", imageMatches);
@@ -191,7 +211,7 @@ public class Hello {
 	// === Display functions ===
 		
 	private static void displayGrayImg() {
-		Mat image = loadImg();
+		Mat image = loadImgLena();
 		Mat thresh = putInGray(image);
 		
 		displayImg("Display Gray", thresh);
@@ -199,7 +219,7 @@ public class Hello {
 	
 	// === Globals ===
 	
-	private static Mat loadImg() {
+	private static Mat loadImgLena() {
 		String	imgName	=	"lena.jpg";	
 		Mat	image	=	imread(imgName);
 		
@@ -209,10 +229,20 @@ public class Hello {
 		
 		return image;
 	}
+	
+	private static Mat loadImg(String imageName) {
+		Mat	image	=	imread(imageName);
+		
+		if	(image.empty())	{
+			throw	new	RuntimeException("cannot	find	img	"	+	imageName	+	"	in	classpath");
+		}
+		
+		return image;
+	}
 
 	private static Mat loadThresh(Mat image) {
 		Mat thresh = new Mat(image.size());
-		threshold(image,thresh,120,255,THRESH_BINARY);
+		//threshold(image,thresh,120,255,THRESH_BINARY);
 		
 		return thresh;
 	}
